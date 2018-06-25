e7e219fed063e1ba4e12ec54cc2e9aa5f70ccee6
Marcus Legendre <marcus.legendre@gmail.com>
2018-06-25 15:07:13 +0200

    Made incubator-openwhisk-deploy-kube a git submodule

--- 3deploy-openwhisk.sh
+++ 3deploy-openwhisk.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-kubectl create namespace openwhisk
-
-cd /tmp
-rm -rf incubator-openwhisk-deploy-kube/
-git clone https://github.com/apache/incubator-openwhisk-deploy-kube
-cd incubator-openwhisk-deploy-kube/helm/
 kubectl label nodes $(kubectl get nodes | grep node | cut -d " " -f 1) openwhisk-role=invoker
-
+kubectl create namespace openwhisk
+helm install incubator-openwhisk-deploy-kube/helm/ --namespace openwhisk --name openwhisk -f values-openwhisk.yaml
 kubectl -n openwhisk apply -f openwhisk-ingress.yaml
