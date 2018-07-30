a66e7a1fab2972de1e6467129a93ec04e7cfaeb2
Marcus Legendre <marcus.legendre@gmail.com>
2018-07-31 01:10:45 +0200

    Check is OpenWhisk CLI is installed

--- configure-wsk.sh
+++ configure-wsk.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if ! which wsk &>/dev/null; then
+    echo "please download and install OpenWhisk CLI first"
+    exit 1
+fi
+
 wsk property set --apihost openwhisk.kubernetes.riotfork.com
 wsk property set --auth 23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP
