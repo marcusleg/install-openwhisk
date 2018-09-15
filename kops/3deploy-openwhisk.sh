b6f654518446f48c5a48922f26a13757950ec88f
Marcus Legendre <marcus.legendre@gmail.com>
2018-09-15 22:22:24 +0200

    kops: simplified values-openwhisk.yaml + ingress is now configured the more canonical way

--- kops/3deploy-openwhisk.sh
+++ kops/3deploy-openwhisk.sh
@@ -2,4 +2,3 @@
 kubectl label nodes $(kubectl get nodes | grep node | cut -d " " -f 1) openwhisk-role=invoker
 kubectl create namespace openwhisk
 helm install ../incubator-openwhisk-deploy-kube/helm/openwhisk/ --namespace openwhisk --name openwhisk -f values-openwhisk.yaml
-kubectl -n openwhisk apply -f openwhisk-ingress.yaml
