c989e0359892333a8add96920cfe274d93720fc0
Marcus Legendre <marcus.legendre@gmail.com>
2018-09-16 09:24:51 +0200

    minikube: start scripts checks if VM is already running and checks if it already exists before setting up a new one

--- minikube/1start-minikube.sh
+++ minikube/1start-minikube.sh
@@ -4,16 +4,29 @@ set -e
 MINIKUBE_VERSION=$(minikube version | cut -d ' ' -f 3)
 if [ $MINIKUBE_VERSION != v0.28.2 ]; then
     echo "This script was written for Minikube v0.28.2. Your version is $MINIKUBE_VERSION."
-    read -p "Do you want to proceed? (y/n)" PROCEED
-    if [ $PROCEED != 'y' ]; then
-        exit
+    read -p "Do you want to proceed? (y/n) " -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        echo ""
+        exit 0
     fi
 fi
 
 minikube profile openwhisk
-minikube config set cpus 4
-minikube config set memory 4096
-minikube start
-minikube ssh -- sudo ip link set docker0 promisc on
-minikube addons enable ingress
-minikube addons enable metrics-server
+
+if minikube status &>/dev/null; then
+    echo 'Minikube already running. Type "minikube stop" to spot it.'
+    exit 1
+fi
+
+if [[ -d ~/.minikube/machines/openwhisk/ ]]; then
+    echo 'Starting existing Minikube VM'
+    minikube start
+else
+    echo 'Setting up new Minikube VM'
+    minikube config set cpus 4
+    minikube config set memory 4096
+    minikube start
+    minikube ssh -- sudo ip link set docker0 promisc on
+    minikube addons enable ingress
+    minikube addons enable metrics-server
+fi
