6e32cb8f44a6ca5416bf61a705f9477189083f19
Marcus Legendre <marcus.legendre@gmail.com>
2018-09-21 00:38:21 +0200

    minikube: fixed OpenWhisk ingress not working when Minikube IP != 192.168.99.100

--- minikube/3deploy-openwhisk.sh
+++ minikube/3deploy-openwhisk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl label nodes --all openwhisk-role=invoker
 kubectl create namespace openwhisk
-helm install ../incubator-openwhisk-deploy-kube/helm/openwhisk/ --namespace openwhisk --name openwhisk -f values-openwhisk.yaml --set ingress.api_host_name=$(minikube ip)
+helm install ../incubator-openwhisk-deploy-kube/helm/openwhisk/ --namespace openwhisk --name openwhisk -f values-openwhisk.yaml --set whisk.ingress.api_host_name=$(minikube ip)
